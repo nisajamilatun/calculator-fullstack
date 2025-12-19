d63aec24e96524e4a7213293f09d90fe8e3276ce
jamilatunnisa <63391873+jamilatunnisa@users.noreply.github.com>
2025-12-19 12:35:48 +0700

    making it in one function: operators

--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,21 @@
 # BASIC CALCULATOR (+, -, x, /) for two numbers
-#addition (+)
-def addition(number1, number2):
-    return number1 + number2
+#operators (+, -, x, /)
+def operators(number1, number2, button):
+    if button == "+":
+        return number1 + number2
+    elif button == "-":
+        return number1 - number2
+    elif button == "*":
+        return number1 * number2
+    elif button == "/":
+        return number1 / number2
+    else:
+        return "Error! input operator +, -, *, /"
 
-#subtraction (-)
-def subtraction(number1,number2):
-    return number1 - number2
-
-#multiplication (x)
-def multiplication(number1, number2):
-    return number1 * number2
-
-#division (/)
-def division(number1, number2):
-    return number1 / number2
 
 
 number1 = int(input("Input number:"))
+button = input("Input button:")
 number2 = int(input("Input number:"))
 
-# print("result:", operators(number1, number2, button))
-
-print(addition(number1, number2)) #addition
+print("result:", operators(number1, number2, button))
